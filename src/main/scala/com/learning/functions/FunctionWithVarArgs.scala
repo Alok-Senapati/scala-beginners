1b74e922c8bd02b525f39243a90e4d211bb2c413
Alok Senapati <asenapati@apple.com>
2023-11-24 10:51:41 +0530

    Scala Learning - Beginner

--- src/main/scala/com/learning/functions/FunctionWithVarArgs.scala
+++ src/main/scala/com/learning/functions/FunctionWithVarArgs.scala
@@ -2,8 +2,11 @@ package com.learning.functions
 
 object FunctionWithVarArgs extends App {
   def sum(nums: Int*): Int = {
+    println(nums.getClass)
     nums.sum
   }
 
-  println(sum(1, 2, 3, 4, 5))
+  val arr: Array[Int] = Array[Int](2, 3, 4, 5, 6)
+  println(sum(arr:_*))
+  println(sum(4, 3, 4, 10, 8))
 }
